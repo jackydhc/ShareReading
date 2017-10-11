d7931310dc165adcb678d4632b2e25781652ee67
jackydhc <dhc52342@163.com>
2017-10-11 21:25:38 +0800

    change the main activity

--- app/src/main/java/com/jacky/sharereading/MainActivity.java
+++ app/src/main/java/com/jacky/sharereading/MainActivity.java
@@ -2,13 +2,16 @@ package com.jacky.sharereading;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout layoutContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        layoutContainer = (FrameLayout) findViewById(R.id.layout_container);
     }
 }
