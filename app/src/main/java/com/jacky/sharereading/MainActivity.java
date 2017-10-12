aef8082fceaac63afc0d600927a7b5fcd399cd27
jackydhc <dhc52342@163.com>
2017-10-12 21:53:08 +0800

    jackydu commit for the addition of firebase

--- app/src/main/java/com/jacky/sharereading/MainActivity.java
+++ app/src/main/java/com/jacky/sharereading/MainActivity.java
@@ -17,6 +17,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         layoutContainer = (FrameLayout) findViewById(R.id.layout_container);
         FirebaseCrash.zzUL();
-
+        FirebaseCrash.log("first log");
     }
 }
